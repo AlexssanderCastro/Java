92b8bac137856e98d249efdacd6185b059a2c173
Alexssander José de Oliveira de Castro <147998833+alexssander77@users.noreply.github.com>
2024-10-21 20:11:15 -0300

    Add files via upload

---
+++ AulaHerança/src/aulaherança/Principal.java
@@ -0,0 +1,47 @@
+package aulaherança;
+
+/**
+ *
+ * @author IFTM
+ */
+public class Principal {
+
+  
+    public static void main(String[] args) {
+        Funcionario joao = new Funcionario();
+        joao.setPrimeiroNome("João");
+        joao.setSegundoNome("Pires");
+        joao.setSalario(900.00);
+        
+        FuncionarioComissionado maria=new FuncionarioComissionado();
+        maria.setPrimeiroNome("Maria");
+        maria.setSegundoNome("Silva");
+        maria.setSalario(1000);
+        maria.setComissao(50);
+        maria.setVendas(10);
+        
+        /***********************************************************************/
+        
+        
+        System.out.println("Funcionario fixo: "+joao.getPrimeiroNome()+" "+joao.getSegundoNome()+"\n"+"Salario: R$"+joao.getSalario());
+        
+         
+        /***********************************************************************/
+        System.out.println("/***********************************************************************/");
+        System.out.println("Funcionario comissionado dia 5: \nNome: "+maria.getPrimeiroNome()+" "+maria.getSegundoNome()+"\nSalario fixo: R$"+maria.getSalario()+"\nPagamento com comissão: R$"+maria.calcularPagamentoTotal());
+        
+        maria.adicionarVendas(5);
+        System.out.println("/***********************************************************************/");
+        System.out.println("Pagamento com comissão: R$"+maria.calcularPagamentoTotal());
+        
+        System.out.println("/***********************************************************************/");
+        maria.zerarVendas();
+        System.out.println("Pagamento com comissão: R$"+maria.calcularPagamentoTotal());
+        
+        maria.adicionarVendas(25);
+        System.out.println("/***********************************************************************/");
+        System.out.println("Pagamento com comissão: R$"+maria.calcularPagamentoTotal());
+        
+    }
+    
+}
