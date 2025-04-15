d8799073de2439f3df892ea5d020dabf23a97344
Alexssander José de Oliveira de Castro <147998833+alexssander77@users.noreply.github.com>
2025-04-15 12:06:01 -0300

    Add files via upload

---
+++ PrjPrimeiroProjetoPOO2/src/main/java/com/mycompany/prjprimeiroprojetopoo2/PrjPrimeiroProjetoPOO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjprimeiroprojetopoo2;
+
+/**
+ *
+ * @author Alexssander
+ */
+public class PrjPrimeiroProjetoPOO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
