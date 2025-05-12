eb825d07d8a6bb44572f086e83671c0ea5ca7516
Alexssander Castro <147998833+AlexssanderCastro@users.noreply.github.com>
2025-05-12 16:24:35 -0300

    Add files via upload

---
+++ POO2/backup/Prj_Calculadoras_POO2/src/main/java/classes/CalculadoraComum.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author Alexssander
+ */
+public class CalculadoraComum {
+    public double somar(double valor1, double valor2){
+      return valor1 + valor2;  
+    }
+    public double subtrair(double valor1, double valor2){
+        return valor1-valor2; 
+    }
+    public double multiplicar(double valor1, double valor2){
+        return valor1*valor2; 
+    }
+    public double dividir(double valor1, double valor2){
+        return valor1/valor2; 
+    }
+}
