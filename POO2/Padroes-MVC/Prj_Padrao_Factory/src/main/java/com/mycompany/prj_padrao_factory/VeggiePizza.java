908010aa92105b1035678051d968017a19ad6952
alexssander77 <alexssandercastro1@gmail.com>
2025-06-09 22:10:35 -0300

    alteração

---
+++ POO2/Padroes-MVC/Prj_Padrao_Factory/src/main/java/com/mycompany/prj_padrao_factory/VeggiePizza.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prj_padrao_factory;
+
+/**
+ *
+ * @author Alexssander
+ */
+public class VeggiePizza extends Pizza{
+    
+    public VeggiePizza() {
+        name="Veggie Pizza";
+        dough="Crust";
+        sauce="Marinata sauce";
+        toppings.add("Shredded Mozzarella");
+        toppings.add("Grated Parmesan");
+        toppings.add("Diced Onion");
+        toppings.add("Sliced mushrooms");
+        toppings.add("Sliced red pepper");
+        toppings.add("Sliced black oliver");
+    }
+    
+}
---
+++ POO2/Padroes-MVC/Prj_Padrao_factory2/src/main/java/com/mycompany/prj_padrao_factory2/Polo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prj_padrao_factory2;
+
+/**
+ *
+ * @author Alexssander
+ */
+public class Polo implements Car{
+    
+    @Override
+    public void showInfo(){
+        System.out.println("Model: Polo");
+    }
+    
+}
